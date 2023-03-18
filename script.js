97aca817712a115a519429c2edbab47b9d480688
ntimanu <ntihinyukae@gmail.com>
2023-03-18 10:03:57 +0200

    Fix desketop footer display

--- script.js
+++ script.js
@@ -11,63 +11,54 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-// Data from speakers
 const speakers = [
   {
-    name: 'Nia Johnson.',
-    description: 'Senior web developer at Google.',
-    expanded: `Senior web developer at Google, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Eng Michel Stevens.',
+    description: 'Structural engineer',
+    expanded: 'Structural disgne in CS construction company, design buildings and bridges. Work in the aerospace industry, designing jetliners and space stations.',
     image: 'img/profile1.jpeg',
   },
 
   {
-    name: 'Kenneth Williams.',
-    description: 'UI/UX Designer at Kentucky.',
-    expanded: ` Senior UI/UX web Designer and Advicer, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Eng Steve Kennedy.',
+    description: 'Electrical Engineer.',
+    expanded: 'A civil engineer is an engineer with expertise in infrastructure construction and operation. Civil engineers participate in construction projects like highways, railroads, and airports.',
     image: 'img/profile2.jpeg',
   },
 
   {
-    name: 'Mark Ruffalo.',
-    description: 'Senior web developer at Microsoft',
-    expanded: `Senior web developer at Microsoft, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Ruth Bush.',
+    description: 'Architectural Engineer',
+    expanded: 'Architecture Engineer for the design of buildings and structures with an emphasis on aesthetics. Architectural engineering combines building design and aesthetics with a focus on sustainable construction and functionality..',
     image: 'img/profile3.jpeg',
   },
 
   {
-    name: 'Amy Smith.',
-    description: 'UI/UX Designer at Los Angeles.',
-    expanded: ` Senior UI/UX web Designer and Advicer, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Clair Williams.',
+    description: 'Construction engineer.',
+    expanded: ' Construction engineers manage and deliver residential and/or construction projects. They plan construction activities, supervise structural elements, and perform building inspections upon project completion..',
     image: 'img/profile4.jpeg',
   },
 
   {
-    name: 'Lian Adams.',
-    description: 'Senior Product Designer in Germany',
-    expanded: ` Senior Product Designer and developer, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Anna Faris.',
+    description: 'Earthquake Engineer',
+    expanded: ' Earthquake engineering is a specialised field in geotechnical engineering. It is a small niche that provides opportunities in a limited number of countries susceptible to earthquakes.',
     image: 'img/profile5.jpeg',
   },
 
   {
-    name: 'Amanda Cole.',
-    description: 'Backend Developer for Amazon in United States.',
-    expanded: ` Beckend developer and Mentor, delivering exceptional websites with expertise in HTML,
-          CSS, JavaScript, and web development technologies.`,
+    name: 'Steve Bruce.',
+    description: 'Civil Engineer.',
+    expanded: ' A civil engineer is an engineer with expertise in infrastructure construction and operation. Civil engineers participate in construction projects like highways, railroads, and airports.',
     image: 'img/profile6.jpeg',
   },
 ];
 
-// The speaker section target
 const mainSpeakers = document.querySelector('.main-speakers');
 mainSpeakers.style.alignItems = 'center';
 mainSpeakers.style.display = 'grid';
 
-// Expandedd description style
 const descriptionStyle = {
   position: 'relative',
   marginLeft: '10px',
@@ -86,8 +77,6 @@ const chessStyle = {
   left: '11px',
 };
 
-// The image speaker style
-
 const speakersImageStyle = {
   width: '120px',
   marginLeft: '20px',
@@ -95,11 +84,9 @@ const speakersImageStyle = {
   position: 'relative',
 };
 
-// The function that shows all speakers and make them grids on breakpoint
-
 function gridify() {
   if (window.innerWidth >= 768) {
-    mainSpeakers.style.gridTemplateColumns = '1fr 1fr';
+    mainSpeakers.style.gridTemplateColumns = '3fr 3fr';
   } else if (window.innerWidth <= 768) {
     mainSpeakers.style.gridTemplateColumns = '1fr';
   }
@@ -108,21 +95,14 @@ function gridify() {
 const body = document.querySelector('body');
 body.addEventListener('click', gridify);
 
-// The function that creates and append the elements
 const inc = 1;
 function main(count) {
   for (let i = 0; i < count; i += inc) {
-    /* The wrapper that covers all of each speaker
-   elements. This is the master div
-   */
     const overAll = document.createElement('div');
     overAll.style.display = 'flex';
     overAll.style.alignItems = 'center';
     mainSpeakers.appendChild(overAll);
 
-    /* The wrapper div that combines the checker
-    image and the speaker image
-    */
     const pictureWrapper = document.createElement('div');
     pictureWrapper.style.position = 'relative';
     pictureWrapper.style.marginBottom = '30px';
@@ -133,31 +113,23 @@ function main(count) {
     descriptionCon.style.marginBottom = '30px';
     overAll.appendChild(descriptionCon);
 
-    // The pic inside the div element
-
-    // The checker image
     const chess = document.createElement('img');
     chess.setAttribute('class', 'chess');
     chess.src = chessStyle.source;
     Object.assign(chess.style, chessStyle);
     pictureWrapper.appendChild(chess);
 
-    // The speaker image
     const speaker = document.createElement('img');
     speaker.src = speakers[i].image;
     Object.assign(speaker.style, speakersImageStyle);
     pictureWrapper.appendChild(speaker);
 
-    // The information of the speaker
-
-    // name
     const name = document.createElement('h3');
     Object.assign(name.style, descriptionStyle);
     name.innerHTML = speakers[i].name;
     name.style.fontWeight = '600';
     descriptionCon.appendChild(name);
 
-    // Description
     const description = document.createElement('h6');
     Object.assign(description.style, descriptionStyle);
     description.style.color = '#ec5242';
@@ -166,7 +138,6 @@ function main(count) {
     description.innerHTML = speakers[i].description;
     descriptionCon.appendChild(description);
 
-    // The last description text
     const expand = document.createElement('h6');
     Object.assign(expand.style, descriptionStyle);
     expand.style.color = '#272a31';
@@ -175,7 +146,6 @@ function main(count) {
   }
 }
 
-// The see more button that is clicked to reveal the remaining speakers section
 const seemore = document.querySelector('#seemore');
 seemore.addEventListener('click', () => {
   mainSpeakers.innerHTML = '';
@@ -183,8 +153,6 @@ seemore.addEventListener('click', () => {
   main(6);
 });
 
-// The function that automatically adjust the position
-// Of the elements on breakpoint
 if (window.innerWidth >= 768) {
   window.onload = main(4);
   mainSpeakers.style.gridTemplateColumns = '1fr 1fr';
